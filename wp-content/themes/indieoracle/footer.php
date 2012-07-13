2d1428a5fb56019cba8b7038a097aab7f03b3cf8
Igor Boskovic <plavetworld@gmail.com>
2012-07-13 12:42:54 +0200

    ver.1.2
    finished home page

--- wp-content/themes/indieoracle/footer.php
+++ wp-content/themes/indieoracle/footer.php
@@ -13,16 +13,20 @@
 
 <footer>
 <?php get_sidebar( 'footer' ); ?>
-
-<a href="#">Facebook</a>
-<a href="#">Twitter</a>
-<a href="#">Blog</a>
-<a href="#">Myspace</a>
-<a href="#">Youtube</a>
+<ul class="social-icons">
+	<li><a class="snfb" href="#" title="Facebook" target="_blank">Facebook</a></li>
+	<li><a class="sntw" href="#" title="Twitter" target="_blank">Twitter</a></li>
+	<li><a class="snb" href="#" title="Blog" target="_blank">Blog</a></li>
+	<li><a class="snms" href="#" title="Myspace" target="_blank">Myspace</a></li>
+	<li><a class="snyt" href="#" title="Youtube" target="_blank">Youtube</a></li>
+</ul>
 <div class="newsletter-box">
 Subscribe to the quarterly Oracle Newsletter <input type="">
 </div>
+
+<a class="kilmulis" href="http://kilmulis.com" title="Kilmulis Design" target="_blank">Kilmulis Design &copy; 2012</a>
 </footer>
+
 <?php wp_footer(); ?>
 <script src="<?php bloginfo('template_directory'); ?>/js/jquery-1.7.1.min.js"></script>
 <script src="<?php bloginfo('template_directory'); ?>/js/jquery.roundabout.min.js"></script>
--- wp-content/themes/indieoracle/home.php
+++ wp-content/themes/indieoracle/home.php
@@ -8,38 +8,36 @@
  */
 get_header(); ?>
 
-<div id="carousel">
-	    <img src="<?php bloginfo('template_directory'); ?>/images/books/a1p.png" alt="" class="slide" />
-	    <img src="<?php bloginfo('template_directory'); ?>/images/books/r&w-fma.png" alt="" class="slide" />
-	    <img src="<?php bloginfo('template_directory'); ?>/images/books/slide3.png" alt="" class="slide" />
-	    <img src="<?php bloginfo('template_directory'); ?>/images/books/slide4.png" alt="" class="slide" />
-	    <img src="<?php bloginfo('template_directory'); ?>/images/books/slide5.png" alt="" class="slide" />
-	    <img src="<?php bloginfo('template_directory'); ?>/images/books/slide6.png" alt="" class="slide" />
-	    <img src="<?php bloginfo('template_directory'); ?>/images/books/slide7.png" alt="" class="slide" />
+	<div id="carousel">
+	    <img src="<?php bloginfo('template_directory'); ?>/images/books/knjiga_arch.png" alt="" class="slide" />
+	    <img src="<?php bloginfo('template_directory'); ?>/images/books/knjiga_rick.png" alt="" class="slide" />
+	    <img src="<?php bloginfo('template_directory'); ?>/images/books/knjiga_arch.png" alt="" class="slide" />
+	    <img src="<?php bloginfo('template_directory'); ?>/images/books/knjiga_rick.png" alt="" class="slide" />
+	    <img src="<?php bloginfo('template_directory'); ?>/images/books/knjiga_arch.png" alt="" class="slide" />
+	    <img src="<?php bloginfo('template_directory'); ?>/images/books/knjiga_rick.png" alt="" class="slide" />
+	    <img src="<?php bloginfo('template_directory'); ?>/images/books/knjiga_arch.png" alt="" class="slide" />
 	</div>
 
 	<div id="carousel-controls">
-	<span class="control current">Book 1</span>
-	<span class="control">Book 2</span>
-	<span class="control">Book 3</span>
-	<span class="control">Book 4</span>
-	<span class="control">Book 5</span>
-	<span class="control">Book 6</span>
-	<span class="control">Book 7</span>
-</div>
+		<span class="control current">Book 1</span>
+		<span class="control">Book 2</span>
+		<span class="control">Book 3</span>
+		<span class="control">Book 4</span>
+		<span class="control">Book 5</span>
+		<span class="control">Book 6</span>
+		<span class="control">Book 7</span>
+	</div>
 
 	<ul id="carousel-descriptions">
 		<li class="desc current">
-			<h2>The Archangel's 1st Proctor</h2>
-			<h4>The Holy Treasure</h4>
+			<h2 class="archangels_hollytreasure_headline">The Archangel's 1st Proctor</h2>
 			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.
 			</p>
 			<a href="#" class="book-more">More</a>
 		</li>
 		<li class="desc">
-			<h2>Rick & while's Fantastical, Magical Adventures - book 1</h2>
-			<h4>Journey to the Kingdom</h4> 
-			<p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.
+			<h2 class="rw_journey_headline">Rick & while's Fantastical, Magical Adventures - book 1</h2>
+			<p>DA MENJA SE I TEKST :) Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium.
 			</p>
 			<a href="#" class="book-more">More</a>
 		</li>
